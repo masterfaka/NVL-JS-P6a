33c219e560f0a5c5d88ed3730ece8bb5a7601bef
redwardPC <redwardweb@gmail.com>
2019-09-10 21:23:25 +0200

    primer commit js

---
+++ js/primer.js
@@ -0,0 +1,7 @@
+console.log("primer js");
+//alert("Algo pa ke se vea");
+var Nombre="Robert";
+var edad= 27 ;
+var diasSem=new Array("lunes", "martes", "miercokles", "jueves", " viernes");
+ var gravedad= 9.80665;
+var PrimerosImpares=new Array(1,2,3); 
